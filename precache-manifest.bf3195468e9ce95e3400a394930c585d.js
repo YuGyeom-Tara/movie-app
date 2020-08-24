f18c264d46ecbe5b7f63503325af6fa08d1e1b4b
Yugyeom <kyk5680579@yonsei.ac.kr>
2020-08-24 13:21:49 +0900

    Updates

---
+++ precache-manifest.bf3195468e9ce95e3400a394930c585d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "360ceb2e7f11b6cfb293c661d0bdfe7d",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "632d6e8ead36c3a4dbb9",
+    "url": "/movie-app/static/css/main.2412b7c1.chunk.css"
+  },
+  {
+    "revision": "1c1867ee385e1fd15da6",
+    "url": "/movie-app/static/js/2.3a05f077.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.3a05f077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "632d6e8ead36c3a4dbb9",
+    "url": "/movie-app/static/js/main.f0eb892f.chunk.js"
+  },
+  {
+    "revision": "8c0fe60c15a349e095c4",
+    "url": "/movie-app/static/js/runtime-main.1fbae34b.js"
+  }
+]);
